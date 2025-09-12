c116396611cb2b15e3a62489033f79888a869388
Andrés Hoyos <andres.hoyos@naranjax.com>
2025-09-12 00:31:30 -0300

    Initial version of VideoStore kata: classes and tests as per Martin Fowler

---
+++ VideoStore/src/VideoStore.Models/Movie.cs
@@ -0,0 +1,8 @@
+namespace VideoStore.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int PriceCode { get; set; }
+    }
+}
---
+++ VideoStore/tests/VideoStore.Models.Tests/CustomerTests.cs
@@ -0,0 +1,82 @@
+using System.Collections.Generic;
+using VideoStore.Models;
+using Xunit;
+
+namespace VideoStore.Models.Tests
+{
+    public class CustomerTests
+    {
+        [Fact]
+        public void Statement_NoRentals_ReturnsZeroAmountAndPoints()
+        {
+            var customer = new Customer { Name = "John" };
+            var expected = "Rental Record for John\nAmount owed is 0\nYou earned 0 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+
+        [Fact]
+        public void Statement_OneRegularRental_TwoDays()
+        {
+            var customer = new Customer { Name = "John" };
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "The Godfather", PriceCode = 0 }, DaysRented = 2 });
+            var expected = "Rental Record for John\n\tThe Godfather\t2\nAmount owed is 2\nYou earned 1 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+
+        [Fact]
+        public void Statement_OneRegularRental_FourDays()
+        {
+            var customer = new Customer { Name = "John" };
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "The Godfather", PriceCode = 0 }, DaysRented = 4 });
+            var expected = "Rental Record for John\n\tThe Godfather\t5\nAmount owed is 5\nYou earned 1 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+
+        [Fact]
+        public void Statement_OneNewReleaseRental_OneDay()
+        {
+            var customer = new Customer { Name = "John" };
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "Avengers", PriceCode = 1 }, DaysRented = 1 });
+            var expected = "Rental Record for John\n\tAvengers\t3\nAmount owed is 3\nYou earned 1 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+
+        [Fact]
+        public void Statement_OneNewReleaseRental_TwoDays()
+        {
+            var customer = new Customer { Name = "John" };
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "Avengers", PriceCode = 1 }, DaysRented = 2 });
+            var expected = "Rental Record for John\n\tAvengers\t6\nAmount owed is 6\nYou earned 2 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+
+        [Fact]
+        public void Statement_OneChildrenRental_ThreeDays()
+        {
+            var customer = new Customer { Name = "John" };
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "Frozen", PriceCode = 2 }, DaysRented = 3 });
+            var expected = "Rental Record for John\n\tFrozen\t1.5\nAmount owed is 1.5\nYou earned 1 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+
+        [Fact]
+        public void Statement_OneChildrenRental_FiveDays()
+        {
+            var customer = new Customer { Name = "John" };
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "Frozen", PriceCode = 2 }, DaysRented = 5 });
+            var expected = "Rental Record for John\n\tFrozen\t4.5\nAmount owed is 4.5\nYou earned 1 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+
+        [Fact]
+        public void Statement_MultipleRentals()
+        {
+            var customer = new Customer { Name = "John" };
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "The Godfather", PriceCode = 0 }, DaysRented = 3 });
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "Avengers", PriceCode = 1 }, DaysRented = 2 });
+            customer.Rentals.Add(new Rental { Movie = new Movie { Title = "Frozen", PriceCode = 2 }, DaysRented = 4 });
+            var expected = "Rental Record for John\n\tThe Godfather\t3.5\n\tAvengers\t6\n\tFrozen\t3\nAmount owed is 12.5\nYou earned 4 frequent renter points";
+            Assert.Equal(expected, customer.Statement());
+        }
+    }
+}
