27d2a7d576b947087f3c897a5593d596baa8cd3b
Andrés Hoyos <andres.hoyos@naranjax.com>
2025-09-12 01:53:39 -0300

    refactor(customer, rental): move charge calculation to Rental for Feature Envy and Information Expert
    - Added getCharge() method to Rental to calculate rental charge.
    - Updated Customer to use rental.getCharge() instead of calculating charge directly.
    - Fixes 'Feature Envy' code smell and applies Information Expert pattern for better encapsulation and design.

--- VideoStore/src/VideoStore.Models/Rental.cs
+++ VideoStore/src/VideoStore.Models/Rental.cs
@@ -10,4 +10,27 @@ public class Rental
         this.Movie = movie;
         this.DaysRented = daysRented;
     }
+
+    public double getCharge()
+    {
+        double result = 0;
+        switch (this.Movie.PriceCode)
+        {
+            case Movie.REGULAR:
+                result += 2;
+                if (this.DaysRented > 2)
+                    result += (this.DaysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                result += this.DaysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                result += 1.5;
+                if (this.DaysRented > 3)
+                    result += (this.DaysRented - 3) * 1.5;
+                break;
+        }
+
+        return result;
+    }
 }
